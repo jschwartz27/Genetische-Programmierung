d77522e1669f80d0215893e200f51376676278b0
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2019-07-13 14:42:07 +0200

    Update genetic_functions.py

--- genetic_functions.py
+++ genetic_functions.py
@@ -1,18 +1,27 @@
 import random
-from functions import find_subtree
+from functions import find_subtree, generate
 
 
 def crossover(lovers):
     indices = list()
+    sub_trees = list()
     for lover in lovers:
         inds = [i for i, x in enumerate(lover) if x in "+-*/"]
-        indices.append(random.choice(inds))
+        index = random.choice(inds)
+        indices.append(index)
+        sub_trees.append(find_subtree(lover, index))
 
-    l1 = lovers[0][:indices[0]] + lovers[1][indices[1]:]
-    l2 = lovers[1][:indices[1]] + lovers[0][indices[0]:]
+    l1 = (lovers[0][:indices[0]] + sub_trees[1] +
+        lovers[0][indices[0]+len(sub_trees[0]):])
+    l2 = (lovers[1][:indices[1]] + sub_trees[0] +
+        lovers[1][indices[1]+len(sub_trees[1]):])
 
     return [l1, l2]
 
 
+def mutation(chrom):
+    pass
+
+
 def gen_pop(pop_size):
-    return list(map(lambda x: f.generate(0), range(pop_size)))
+    return list(map(lambda x: generate(0), range(pop_size)))
