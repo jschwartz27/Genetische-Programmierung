94d390052cd65cf4ca5aee430ef5336cff24000f
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2020-02-23 14:32:02 +0100

    Update the_function_is.py

--- the_function_is.py
+++ the_function_is.py
@@ -2,6 +2,56 @@ import random
 import functions as f
 import genetic_functions as genetik
 
+
+def create_samples(func, n_vs):
+    sample_n = 3
+    ns = "xyz"[:n_vs+1]
+
+    sample_dict = dict()
+    for i in range(sample_n):
+        vs = {j: random.choice(range(1, 21)) for j in ns}
+        e = f.evaluate_Vs(func, vs)
+        sample_dict[str(vs)] = e
+
+    return sample_dict
+
+
+def evolve_Vs(samples, pop_size, n_gens, mut_rate, elite_perc):
+    pass
+
+
+def generate_f(n_vs, eq_l):
+    vs = random.choices("xyz"[:n_vs+1], k=eq_l)
+    eq = list()
+
+    return eq
+
+
+def main():
+    n_vs = random.choice(range(3))  # 0 = 1 var, etc.
+    n_vs = 0
+    eq_len = random.choice(range(2, 7))  # n vars in eq
+    function = generate_f(n_vs, eq_len)
+    quit()
+
+    pop_size = 1000
+    n_gens = 2000
+    mut_rate = .1
+    elite_perc = .1
+    n_vs = random.choice(range(1, 4))
+    function = random.choice(functions[str(n_vs)])
+    sample_dict = create_samples(function, n_vs)
+
+    result = evolve_Vs(sample_dict, pop_size, n_gens, mut_rate, elite_perc)
+
+    print("The function is:: {}".format(function))
+    print("Trained with the following data points::")
+    print("\t{}".format(sample_dict))
+
+if __name__ == '__main__':
+    main()
+
+'''
 functions = {
     "1": [
         "**xxx",      # x**3
@@ -20,38 +70,4 @@ functions = {
         "++*xx*yyz"   # x**2 + y**2 + z
     ]
 }
-
-
-def create_samples(func, n_vs):
-    sample_n = 3
-    ns = "xyz"
-    sample_dict = dict()
-    for i in range(sample_n):
-        vs = {j: random.choice(range(1, 21)) for j in ns[:n_vs]}
-        e = f.evaluate_Vs(func, vs)
-        sample_dict[str(vs)] = e
-
-    return sample_dict
-
-
-def evolve_Vs(samples, pop_size, n_gens, mut_rate, elite_perc):
-    pass
-
-
-def main():
-    pop_size = 1000
-    n_gens = 2000
-    mut_rate = .1
-    elite_perc = .1
-    n_vs = random.choice(range(1, 4))
-    function = random.choice(functions[str(n_vs)])
-    sample_dict = create_samples(function, n_vs)
-
-    result = evolve_Vs(sample_dict, pop_size, n_gens, mut_rate, elite_perc)
-
-    print("The function is:: {}".format(function))
-    print("Trained with the following data points::")
-    print("\t{}".format(sample_dict))
-
-if __name__ == '__main__':
-    main()
+'''
